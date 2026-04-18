3c4b54fe8650d3c48904d2e0f042f4332bad4491
bjavohir2197 <bjavohir2197@gmail.com>
2026-04-18 12:26:03 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+        print("=" * 40)
+    print("Masala: Satrni teskari qiling, lekin sozlar tartibi ozgarmasin (masalan, "Hello world" → "olleH dlrow").")
+    print("=" * 40)
+    print("\nDastur muvaffaqiyatli ishga tushdi!")
+    print("Masala yechildi.")
+
+
+if __name__ == "__main__":
+    main()
